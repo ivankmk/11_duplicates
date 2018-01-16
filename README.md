f7e1f7917bb5d5f2d17cf185b6e9b01acc72f63f
ivankmk <kumeyko@hotmail.com>
2018-01-16 23:42:25 +0100

    script created

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Script is analyzing the directory and revert all duplicated files in it.
+
+Duplicated means - at least two files with same name and size.
+
+# Requirements
+
+ - Python 3.5;
+
+# How to launch
+
+```bash
+$ python duplicates.py <directory> # possibly requires call of python3 executive instead of just python
+Hello, below files which at least duplicated:
+----------------------------------------
+naked_ex.png :  4
+cat.png :  2
+----------------------------------------
+
+```
+
+The same with Windows environment;
+
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,37 @@
+import os
+import sys
+from collections import Counter
+
+
+def get_list_of_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+
+        if '/.' not in root:
+            for file_data in files:
+                path = os.path.join(root, file_data)
+                if '/.' not in path:
+                    size = os.stat(path).st_size
+                    all_files.append((path.split('/')[-1], size))
+    return all_files
+
+
+def get_duplications(files):
+    duplicated_files = []
+    files_with_counter = Counter(files)
+    for name_size, count in files_with_counter.items():
+        if count > 1:
+            duplicated_files.append((name_size[0], name_size[1], count))
+    return duplicated_files
 
 if __name__ == '__main__':
-    pass
+    try:
+        data = get_list_of_files(sys.argv[1])
+    except IndexError:
+        sys.exit('Please, enter correct path.')
+    duplicated_result = get_duplications(data)
+    print('Hello, below files which at least duplicated:')
+    print('-'*40)
+    for filename, _, count in duplicated_result:
+        print(filename, ': ', count)
+    print('-'*40)
