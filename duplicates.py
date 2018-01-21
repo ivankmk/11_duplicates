328df436d9482f51bd1129645e4d18007b65341c
ivankmk <kumeyko@hotmail.com>
2018-01-21 21:10:20 +0100

    script amended

--- duplicates.py
+++ duplicates.py
@@ -3,9 +3,7 @@ import collections
 import os.path
 from collections import Counter
 
-# #        if os.path.exists(sys.argv[1]):
-#         else:
-#             print('Folder path not exist.')
+
 def get_locations(directory):
     if os.path.exists(directory):
         file_locations = collections.defaultdict(list)
@@ -13,18 +11,18 @@ def get_locations(directory):
             for file_name in file_names:
                 path_to_current_file = os.path.join(rootdir, file_name)
                 file_size = os.path.getsize(path_to_current_file)
-                file_locations[(file_name, file_size)].append(path_to_current_file)
+                file_locations[(file_name,
+                                file_size)].append(path_to_current_file)
         return file_locations
     else:
         return None
+
 
 def get_duplications(all_files):
     return [((file_name, size), paths) for (file_name, size),
             paths in all_files.items() if len(paths) > 1]
 
-# #        if os.path.exists(sys.argv[1]):
-#         else:
-#             print('Folder path not exist.')
+
 if __name__ == '__main__':
     try:
         if get_locations(sys.argv[1]) is None:
@@ -37,6 +35,6 @@ if __name__ == '__main__':
             for (file_name, size), paths in duplicated_files:
                 print(file_name)
                 for path in paths:
-                   print('    ', path)
+                    print('    ', path)
     except IndexError:
         sys.exit('Please, enter the path.')
